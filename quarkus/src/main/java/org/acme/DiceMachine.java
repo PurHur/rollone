61fd7f80ab8f7795a0d8a199d75c9386f69e6617
Philip Maaß <PhilipMaasz@aol.com>
2022-04-22 17:17:43 +0200

    removes empty lines

--- quarkus/src/main/java/org/acme/DiceMachine.java
+++ quarkus/src/main/java/org/acme/DiceMachine.java
@@ -2,18 +2,14 @@ package org.acme;
 import java.util.Random;
 
 public class DiceMachine {
-
     public int[] dice(int sides, int roles){
-
         int[] rolled ={};
 
         for(int i=1;i<roles;i++){
             Random random = new Random();
             rolled[i] = random.nextInt(sides);
         }
+        
         return rolled;
     }
-
-
-
 }
