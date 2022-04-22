13769d38c3c2086d17e2e758ce3a0e6def0cb38b
mamichels <info@marcelmichels.de>
2022-04-22 12:08:20 +0200

    Sick cube shader

--- frontend/main.js
+++ frontend/main.js
@@ -11,23 +11,30 @@ const initDice = () => {
     document.body.appendChild( renderer.domElement );
 
     const scene = new THREE.Scene();
+    // set scene background color
+    scene.background = new THREE.Color( 0xffffff );
+    
     const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
     camera.position.set( 0, 0, 100 );
     camera.lookAt( 0, 0, 0 );
 
     const geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    const material = new THREE.MeshBasicMaterial( {
-        color: 0x00ffaa,
-        wireframe: false,
-        vertexColors: THREE.FaceColors
-    } );
+    const material = new THREE.ShaderMaterial({
+        uniforms: {
+            size: {
+                value: new THREE.Vector3(geometry.parameters.width, geometry.parameters.height, geometry.parameters.depth).multiplyScalar(0.5)
+            },
+            thickness: {
+                value: 0.01
+            },
+            smoothness: {
+                value: 0.2
+            }
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
     const cube = new THREE.Mesh( geometry, material );
-
-    cube.geometry.faces[0].color.setHex( 0x0000ff );
-    cube.geometry.faces[1].color.setHex( 0x0000ff );
-
-    cube.geometry.faces[2].color.setHex( 0xccffaa );
-    cube.geometry.faces[3].color.setHex( 0xccffaa );
 
     scene.add( cube );
 
@@ -39,3 +46,30 @@ const initDice = () => {
     }, 1000/60);
 
 }
+
+const vertexShader = `
+    varying vec3 vPos;
+    void main()	{
+      vPos = position;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+    }
+  `;
+
+const fragmentShader = `
+
+    varying vec3 vPos;
+    uniform vec3 size;
+    uniform float thickness;
+    uniform float smoothness;
+   
+    void main() {
+			
+      float a = smoothstep(thickness, thickness + smoothness, length(abs(vPos.xy) - size.xy));
+      a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.yz) - size.yz));
+      a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.xz) - size.xz));
+      
+      vec3 c = mix(vec3(1), vec3(0), a);
+      
+      gl_FragColor = vec4(c, 1.0);
+    }
+  `;
