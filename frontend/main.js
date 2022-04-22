0d2f760aa56bb19e89af4800d56009db02e5f3b7
mamichels <info@marcelmichels.de>
2022-04-22 18:17:42 +0200

    revert

--- frontend/main.js
+++ frontend/main.js
@@ -74,7 +74,7 @@ const initDice = () => {
     dice.getObject().rotation.y = -120 * Math.PI / 180;
     dice.getObject().rotation.x = 45 * Math.PI / 180;
     dice.updateBodyFromMesh();
-    
+
     // Set the value of the side, which will be upside after the dice lands
     DiceManager.prepareValues([{dice: dice, value: 1}]);
 
@@ -107,7 +107,26 @@ const initActions = () => {
 };
 
 const setDiceValue = (value) => {
-    DiceManager.prepareValues([{dice: dice, value: value}]);
+    var diceValues = [];
+
+    for (var i = 0; i < dice.length; i++) {
+        let yRand = Math.random() * 20
+        dice[i].resetBody(); // As the die is going to be reused between throws, it is necessary to reset the body
+        dice[i].getObject().position.x = -15 - (i % 3) * 1.5;
+        dice[i].getObject().position.y = 2 + Math.floor(i / 3) * 1.5;
+        dice[i].getObject().position.z = -15 + (i % 3) * 1.5;
+        dice[i].getObject().quaternion.x = (Math.random()*90-45) * Math.PI / 180;
+        dice[i].getObject().quaternion.z = (Math.random()*90-45) * Math.PI / 180;
+        dice[i].updateBodyFromMesh();
+        let rand = Math.random() * 5;
+        dice[i].getObject().body.velocity.set(25 + rand, 40 + yRand, 15 + rand);
+        dice[i].getObject().body.angularVelocity.set(20 * Math.random() -10, 20 * Math.random() -10, 20 * Math.random() -10);
+
+        diceValues.push({dice: dice[i], value: i + 1});
+    }
+
+    DiceManager.prepareValues(diceValues);
+    // DiceManager.prepareValues([{dice: dice, value: value}]);
 };
 
 const initEventSource = () => {
