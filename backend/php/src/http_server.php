a9065da8d4fc99b5dce31d1b13da0d66c73a8b2f
Andre Michels <amichels@databay.de>
2022-04-22 10:45:43 +0200

    Init work with php

---
+++ backend/php/src/http_server.php
@@ -0,0 +1,39 @@
+<?php
+
+$http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) {
+
+    $httpPath = $request->getUri()->getPath();
+    if ($httpPath === '/') {
+        $httpPath = '/index.html';
+    }
+    $filePath = "../../frontend/".$httpPath;
+
+    if (!file_exists($filePath)) {
+        return new React\Http\Message\Response(
+            React\Http\Message\Response::STATUS_NOT_FOUND
+        );
+    }
+
+    $content = file_get_contents($filePath);
+
+    $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+    if ($extension === 'html') {
+        $mime = "text/html";
+    } else {
+        $mime = mime_content_type($filePath);
+    }
+
+    return new React\Http\Message\Response(
+        React\Http\Message\Response::STATUS_OK,
+        [
+             'Content-Type' => $mime.'; charset=utf-8'
+        ],
+        $content
+    );
+});
+
+$socket = new React\Socket\SocketServer('127.0.0.1:8080');
+
+echo "Listening on {$socket->getAddress()}\n";
+$http->listen($socket);
+
