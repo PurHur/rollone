15d8fb5c5f70ec6c34bd7730ea5fd1570ce9e48f
mamichels <info@marcelmichels.de>
2022-04-23 12:15:07 +0200

    They see me rollin

--- backend/php/src/App/Controller/DiceController.php
+++ backend/php/src/App/Controller/DiceController.php
@@ -34,7 +34,7 @@ class DiceController
     {
         $sides = $request->getQueryParams()['sides'];
         $result = $this->diceService->rollDices($sides);
-        $username = explode('=', implode(',', $request->getHeaders()['Cookie']))[0];
+        $username = explode('=', implode(',', $request->getHeaders()['Cookie']))[1];
 
         return SSEHelper::generateSSEEvent("roll",'{"rolls": '.json_encode($result).', "sides": '.$sides.', "roller": "'.$username.'"}');
     }
