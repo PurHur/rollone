b2d147587dbbd2a058a0ce9874bdb10520da7b29
Andre Michels <amichels@databay.de>
2022-04-23 18:27:03 +0200

    Fadeout effect

--- frontend/main.js
+++ frontend/main.js
@@ -203,6 +203,8 @@ const initEventSource = () => {
         console.log('roll', event);
 
         document.getElementById("journal").innerHTML += '<div class="journal-entry">'+data.roller+' würfelt mit einem d'+data.sides+' '+data.rolls.join(",")+'</div>';
+
+        fadeOut(".journal-entry",1000)
     });
     eventSource.addEventListener('name', (event) => {
         const name = event.data;
@@ -210,6 +212,25 @@ const initEventSource = () => {
         document.getElementById("username").value = name;
     });
 };
+
+function fadeOut(selector, timeInterval, callback = null) {
+
+    var fadeTarget = document.querySelector(selector);
+
+    let time = timeInterval / 1000;
+    fadeTarget.style.transition = time + 's';
+    fadeTarget.style.opacity = 0;
+    var fadeEffect = setInterval(function() {
+
+        if (fadeTarget.style.opacity <= 0)
+        {
+            clearInterval(fadeEffect);
+            if (typeof (callback) === 'function') {
+                callback();
+            }
+        }
+    }, timeInterval);
+}
 
 const getRequest = (url) => {
     return new Promise((resolve, reject) => {
