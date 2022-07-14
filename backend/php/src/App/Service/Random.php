e4001e894e524cbc0dca6d327462fd938311593c
Philip Maaß <PhilipMaasz@aol.com>
2022-07-14 20:31:19 +0200

    changes the random number generation

--- backend/php/src/App/Service/Random.php
+++ backend/php/src/App/Service/Random.php
@@ -13,16 +13,11 @@ class Random
 {
     /**
      * Fetches a random number for using as a seed from an external source.
-     * URL: http://www.randomnumberapi.com/api/v1.0/random?min=1000&max=1000000000&count=1
      *
      * @return int
      */
     public static function getSeed() : int
     {
-        $url = 'http://www.randomnumberapi.com/api/v1.0/random?min=1000&max=1000000000&count=1';
-        $json = file_get_contents($url);
-        $data = json_decode($json, true);
-
-        return current($data);
+        return (int)hexdec(md5(file_get_contents("https://sdo.gsfc.nasa.gov/assets/img/latest/latest_4096_0171.jpg")));
     }
 }
